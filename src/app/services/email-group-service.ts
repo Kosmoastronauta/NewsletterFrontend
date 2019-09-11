61bd0c713cb65ca389d4fdd92193259cb7b92bf2
Mateusz Gil <mateusz_gil@outlook.com>
2019-09-11 15:33:57 +0200

    Implemented get all email groups in service

--- src/app/services/email-group-service.ts
+++ src/app/services/email-group-service.ts
@@ -1,2 +1,16 @@
+import {HttpClient} from '@angular/common/http';
+import {EmailGroup} from '../models/email-group';
+import {Observable} from 'rxjs';
+
 export class EmailGroupService {
+
+    private BASE_URL = 'http://localhost:8080';
+    private ALL_EMAIL_GROUPS_URL = `${this.BASE_URL}\\emailGroups\\`;
+
+    constructor(private http: HttpClient) {}
+
+    getAllEmailGroups(): Observable<EmailGroup[]>
+    {
+        return this.http.get<EmailGroup[]>(this.ALL_EMAIL_GROUPS_URL);
+    }
 }
